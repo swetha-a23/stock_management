87d466ffc8470972762b3054fcd5826640f5592c
Swetha <swethaswe2306@gmail.com>
2023-07-15 13:05:35 +0530

    updated content

--- database.py
+++ database.py
@@ -2,22 +2,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import declarative_base
 
+from sqlalchemy.orm import scoped_session
+
 Base = declarative_base()
 
 DATABASE_URI = "postgresql://postgres:database@database-1.cqv5dapod8ah.us-east-1.rds.amazonaws.com:5432/sms_lambda"
 # Create the engine
 engine = create_engine(DATABASE_URI)
-Session = sessionmaker(bind=engine)
-session=Session()
-
-
-
-
-def init_database():
-    from models import Consumer, Supplier, Product, Stock, ConsumerOrder, SupplierOrder, ConsumerTransaction, SupplierTransaction
-    session = Session()
-    Base.metadata.drop_all(bind=engine)
-    Base.metadata.create_all(bind=engine)
-    session.commit()
-    session.close()
+session=scoped_session(sessionmaker(autocommit=False,autoFlush=False,bind=Engine))
 

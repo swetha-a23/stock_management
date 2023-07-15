e2795506da452e5fd8d3972bd7f91713857ddddc
Swetha <swethaswe2306@gmail.com>
2023-07-15 12:38:57 +0530

    updated content

--- database.py
+++ database.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
 
-DATABASE_URI = "postgresql://postgres:database@database-1.cqv5dapod8ah.us-east-1.rds.amazonaws.com:5432/sms_aws"
+DATABASE_URI = "postgresql://postgres:database@database-1.cqv5dapod8ah.us-east-1.rds.amazonaws.com:5432/sms_lambda"
 # Create the engine
 engine = create_engine(DATABASE_URI)
 Session = sessionmaker(bind=engine)

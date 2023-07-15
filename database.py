3acddd252847e528b6b3f598b1c0651358c8f2a4
Swetha <swethaswe2306@gmail.com>
2023-07-15 13:11:00 +0530

    updated content

--- database.py
+++ database.py
@@ -9,5 +9,5 @@ Base = declarative_base()
 DATABASE_URI = "postgresql://postgres:database@database-1.cqv5dapod8ah.us-east-1.rds.amazonaws.com:5432/sms_lambda"
 # Create the engine
 engine = create_engine(DATABASE_URI)
-session=scoped_session(sessionmaker(autocommit=False,autoFlush=False,bind=Engine))
+session=scoped_session(sessionmaker(autocommit=False,autoFlush=False,bind=engine))
 
